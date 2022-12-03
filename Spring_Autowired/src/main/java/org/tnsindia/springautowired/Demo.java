3282c43efde5f4780256738cdb34c3aee9eb0b95
Rutvik_Sakpal <Rutvik_Sakpal@LAPTOP-CCS0N10J>
2022-12-03 22:49:25 +0530

    First Commit

---
+++ Spring_Autowired/src/main/java/org/tnsindia/springautowired/Demo.java
@@ -0,0 +1,15 @@
+package org.tnsindia.springautowired;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+//driver class
+public class Demo {
+
+	public static void main(String[] args) {
+		ApplicationContext c=new ClassPathXmlApplicationContext("beans.xml");
+		Human human=c.getBean("human",Human.class);
+		human.pumping();
+
+	}
+
+}
---
+++ Spring_Autowired/src/main/java/org/tnsindia/springautowired/Heart.java
@@ -0,0 +1,43 @@
+package org.tnsindia.springautowired;
+//class
+public class Heart {
+	
+	//Dependency in terms of literals
+	private String nameOfAnimal;
+	private int noOfHeart;
+	
+	//getters and setters
+	
+	public String getNameOfAnimal() {
+		return nameOfAnimal;
+	}
+
+
+
+	public void setNameOfAnimal(String nameOfAnimal) {
+		this.nameOfAnimal = nameOfAnimal;
+	}
+
+
+
+	public int getNoOfHeart() {
+		return noOfHeart;
+	}
+
+
+
+	public void setNoOfHeart(int noOfHeart) {
+		this.noOfHeart = noOfHeart;
+	}
+
+
+
+	//method
+	public void pump()
+	{
+		System.out.println("Your heart is pumping");
+		System.out.println("You are alive!");
+	}
+	
+
+}
